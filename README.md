2a760416710f5b08f063cd76bcaebd9dbdb0ae4f
Suguru Kouchi <e155742@ie.u-ryukyu.ac.jp>
2017-06-07 13:32:13 +0900

    add HsvConverter class

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ETロボコン EV3 HackEV
 ====
 ### 現在の状態
+RGBをHSVに変換するHsvConverterクラス追加  
 初期位置コードをデコードするDecoderクラス追加
 msg_fからVarEnteringクラスを使用できるように変更  
 ボタン入力するVarEnteringクラス追加  
--- app.cpp
+++ app.cpp
@@ -1,22 +1,20 @@
 #include <vector>
 #include "app.h"
 #include "util.h"
-#include "decoder.hpp"
+#include "hsv_converter.hpp"
 
 void main_task(intptr_t unused) {
-    std::vector<int> pos = {0, 0, 0, 0, 0};
-    inputInt(pos, "Enter Init position code");
+    std::vector<rgb_t> rgb = {0, 0, 0};
+    inputInt(rgb, "Enter RGB");
     msg_clear();
-    Decoder decoder(pos[4] + pos[3] * 10 + pos[2] * 100 + pos[1] * 1000 + pos[0] * 10000);
+    HsvConverter hsv(rgb[0], rgb[1], rgb[2]);
+    msg_f("HSV color", 1);
     char str[64];
-    msg_f("Color block position", 1);
-    sprintf(str, "Black  %d", decoder.getBlackPosition());
+    sprintf(str, "Hue        %f", hsv.getHue());
     msg_f(str, 3);
-    sprintf(str, "Red    %d", decoder.getRedPosition());
+    sprintf(str, "Saturation %f", hsv.getSaturation());
+    msg_f(str, 4);
+    sprintf(str, "Value      %f", hsv.getLightness());
     msg_f(str, 5);
-    sprintf(str, "Yellow %d", decoder.getYellowPosition());
-    msg_f(str, 7);
-    sprintf(str, "Blue   %d", decoder.getBluePosition());
-    msg_f(str, 9);
     while(true) {;}
 }
