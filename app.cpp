5f468172fcb9d243335bec080d2b44a3120c6fd2
Suguru Kouchi <e155742@ie.u-ryukyu.ac.jp>
2017-06-07 13:01:29 +0900

    add VarEntering class

--- app.cpp
+++ app.cpp
@@ -1,10 +1,14 @@
 #include "app.h"
 #include "util.h"
+#include "var_entering.hpp"
 
 void main_task(intptr_t unused) {
+    int n = 3;
+    VarEntering entering;
+    entering.inputInt(n, "VarEntering Class");
     const char* hello = "Hello";
     msg_f(hello, 1);
     msg_f("EV", 2);
-    msg_f(3, 3);
+    msg_f(n, 3);
     while(true) {;}
 }
